baba03495e152a6486e4ca0873baf7e6b2fe2511
Enrico Nello <nello.enrico@gmail.com>
2024-02-03 16:43:55 +0000

    updated deepfake_generation script

--- 3_image_generation/deepfake_generation.py
+++ 3_image_generation/deepfake_generation.py
@@ -1,8 +1,6 @@
 import os
 import torch
 import time
-import numpy as np
-import matplotlib.pyplot as plt
 from diffusers import StableDiffusionPipeline
 import pandas as pd
 from torch.cuda.amp import autocast
@@ -13,6 +11,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 load_dotenv()
 
 NUM_IMAGES = int(os.getenv("NUM_IMAGES"))
+START_INDEX = int(os.getenv("START_INDEX"))
+END_INDEX = int(os.getenv("END_INDEX"))
 BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
 NUM_INFERENCE_STEPS = int(os.getenv("NUM_INFERENCE_STEPS"))
 INPUT_CSV_PATH = os.getenv("INPUT_CSV_PATH")
@@ -57,20 +57,30 @@ class Txt2ImgGenerator:
         for i in range(0, len(lst1), chunk_size):
             yield lst1[i:i + chunk_size], lst2[i:i + chunk_size]
 
-    def generate_images(self):
-        total_images = len(self.prompts[:NUM_IMAGES])
-        batches = list(self.divide_chunks(self.prompts[:NUM_IMAGES], self.images_ids[:NUM_IMAGES], self.batch_size))
+    def generate_images(self, start_index, end_index):
+        
+        prompts_slice = self.prompts[start_index:end_index]
+        images_ids_slice = self.images_ids[start_index:end_index]
+
+        total_images = len(prompts_slice)
+        batches = list(self.divide_chunks(prompts_slice, images_ids_slice, self.batch_size))
+
         print("Total prompts:", total_images)
         counter = 0
 
         start_time = time.time()
 
+        # negative prompts must match the batch size of prompts
+        #negative_prompts_batch = [NEGATIVE_PROMPTS] * self.batch_size
+
         for index, (batch_prompts, batch_image_ids) in enumerate(batches):
             print("Processing batch {}/{}".format(index + 1, len(batches)))
 
+            negative_prompts_batch = [NEGATIVE_PROMPTS] * len(batch_prompts)  # Match batch size
+
             # generate images for the batch of prompts
             with autocast(enabled=True):
-                generated_images = self.model(batch_prompts, negative_prompt=[NEGATIVE_PROMPTS], num_inference_steps=self.num_inference_steps)["images"]
+                generated_images = self.model(batch_prompts, negative_prompt=negative_prompts_batch, num_inference_steps=self.num_inference_steps)["images"]
 
             # save the generated images and update generated_image_info
             for i in range(len(batch_prompts)):
@@ -98,7 +108,7 @@ class Txt2ImgGenerator:
         # Convert generated_image_info to DataFrame and save as new CSV
         generated_info_df = pd.DataFrame(self.generated_image_info)
         input_df = pd.read_csv(INPUT_CSV_PATH)
-        result_df = pd.merge(input_df[:NUM_IMAGES], generated_info_df, on='id', how='left')
+        result_df = pd.merge(input_df.iloc[START_INDEX:END_INDEX], generated_info_df, on='id', how='left')
         result_df['class'] = 'fake' # from 'pristine' now they will be 'fake'
 
         result_df.to_csv(destination_dir, index=False)
@@ -115,4 +125,4 @@ if __name__ == "__main__":
         num_inference_steps=NUM_INFERENCE_STEPS
     )
 
-    SD.generate_images()
+    SD.generate_images(START_INDEX, END_INDEX)
