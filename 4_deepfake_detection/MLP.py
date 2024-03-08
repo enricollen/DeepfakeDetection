40340a26e9d92028693af751ff6a18e8ab549862
Enrico Nello <nello.enrico@gmail.com>
2024-03-08 11:25:28 +0000

    new version of MLP with dropout

--- 4_deepfake_detection/MLP.py
+++ 4_deepfake_detection/MLP.py
@@ -1,6 +1,25 @@
 import torch.nn as nn
 
 class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_layers, output_dim):
+        super(MLP, self).__init__()
+        layers = []
+        prev_dim = input_dim
+        for i, (dim, dropout) in enumerate(hidden_layers):
+            layers.append(nn.Linear(prev_dim, dim))
+            layers.append(nn.ReLU())
+            if i != len(hidden_layers) - 1:  # dropout except for the last layer
+                layers.append(nn.Dropout(p=dropout))
+            prev_dim = dim
+        layers.append(nn.Linear(prev_dim, output_dim))
+        self.mlp = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.mlp(x)
+
+"""import torch.nn as nn
+
+class MLP(nn.Module):
     def __init__(self, input_dim, hidden_dims, output_dim):
         super(MLP, self).__init__()
         layers = []
@@ -13,4 +32,4 @@ class MLP(nn.Module):
         self.mlp = nn.Sequential(*layers)
     
     def forward(self, x):
-        return self.mlp(x)
+        return self.mlp(x)"""
