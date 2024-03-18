b8e9b0108bb8982d51a6ce1e48468a2211bb1abb
Enrico Nello <nello.enrico@gmail.com>
2024-03-18 14:58:31 +0000

    possibility for MLP to specify also Dropout = None after a FC layer

--- 4_deepfake_detection/MLP.py
+++ 4_deepfake_detection/MLP.py
@@ -8,7 +8,7 @@ class MLP(nn.Module):
         for i, (dim, dropout) in enumerate(hidden_layers):
             layers.append(nn.Linear(prev_dim, dim))
             layers.append(nn.ReLU())
-            if i != len(hidden_layers) - 1:  # dropout except for the last layer
+            if dropout is not None and i != len(hidden_layers) - 1:
                 layers.append(nn.Dropout(p=dropout))
             prev_dim = dim
         layers.append(nn.Linear(prev_dim, output_dim))
@@ -16,6 +16,23 @@ class MLP(nn.Module):
     
     def forward(self, x):
         return self.mlp(x)
+    
+"""class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_layers, output_dim):
+        super(MLP, self).__init__()
+        layers = []
+        prev_dim = input_dim
+        for i, (dim, dropout) in enumerate(hidden_layers):
+            layers.append(nn.Linear(prev_dim, dim))
+            layers.append(nn.ReLU())
+            if i != len(hidden_layers) - 1:  # dropout except for the last layer
+                layers.append(nn.Dropout(p=dropout))
+            prev_dim = dim
+        layers.append(nn.Linear(prev_dim, output_dim))
+        self.mlp = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        return self.mlp(x)"""
 
 """import torch.nn as nn
 
